436e10a96dc67bcbee083dd80f40ea4e26099402
volkankoseoglu06-hue <volkankoseoglu06@gmail.com>
2026-08-13 08:48:05 +0300

    Update foods.js

--- foods.js
+++ foods.js
@@ -1,1 +1,246 @@
+const foods = [
 
+{
+name:"Tavuk göğsü",
+unit:"100 g",
+calorie:165,
+protein:31,
+carb:0,
+fat:3.6
+},
+
+{
+name:"Hindi göğsü",
+unit:"100 g",
+calorie:135,
+protein:29,
+carb:0,
+fat:1
+},
+
+{
+name:"Köfte",
+unit:"100 g",
+calorie:250,
+protein:26,
+carb:5,
+fat:15
+},
+
+{
+name:"Dana eti",
+unit:"100 g",
+calorie:250,
+protein:26,
+carb:0,
+fat:15
+},
+
+{
+name:"Ton balığı",
+unit:"100 g",
+calorie:132,
+protein:29,
+carb:0,
+fat:1
+},
+
+{
+name:"Somon",
+unit:"100 g",
+calorie:208,
+protein:20,
+carb:0,
+fat:13
+},
+
+{
+name:"Yumurta",
+unit:"1 adet",
+calorie:78,
+protein:6,
+carb:0.6,
+fat:5
+},
+
+{
+name:"Yoğurt",
+unit:"100 g",
+calorie:61,
+protein:3.5,
+carb:4.7,
+fat:3.3
+},
+
+{
+name:"Ayran",
+unit:"1 bardak",
+calorie:68,
+protein:4,
+carb:5,
+fat:3
+},
+
+{
+name:"Süt",
+unit:"1 bardak",
+calorie:103,
+protein:8,
+carb:12,
+fat:2
+},
+
+{
+name:"Kaşar peyniri",
+unit:"100 g",
+calorie:404,
+protein:25,
+carb:1,
+fat:33
+},
+
+{
+name:"Lor peyniri",
+unit:"100 g",
+calorie:98,
+protein:18,
+carb:3,
+fat:1
+},
+
+{
+name:"Pirinç pilavı",
+unit:"100 g",
+calorie:130,
+protein:2.7,
+carb:28,
+fat:0.3
+},
+
+{
+name:"Bulgur pilavı",
+unit:"100 g",
+calorie:83,
+protein:3,
+carb:18,
+fat:0.2
+},
+
+{
+name:"Makarna",
+unit:"100 g",
+calorie:157,
+protein:5.8,
+carb:31,
+fat:1
+},
+
+{
+name:"Kuru fasulye",
+unit:"100 g",
+calorie:127,
+protein:9,
+carb:23,
+fat:0.5
+},
+
+{
+name:"Nohut",
+unit:"100 g",
+calorie:164,
+protein:9,
+carb:27,
+fat:2.6
+},
+
+{
+name:"Mercimek",
+unit:"100 g",
+calorie:116,
+protein:9,
+carb:20,
+fat:0.4
+},
+
+{
+name:"Patates",
+unit:"100 g",
+calorie:77,
+protein:2,
+carb:17,
+fat:0.1
+},
+
+{
+name:"Muz",
+unit:"1 adet",
+calorie:105,
+protein:1.3,
+carb:27,
+fat:0.4
+},
+
+{
+name:"Elma",
+unit:"1 adet",
+calorie:95,
+protein:0.5,
+carb:25,
+fat:0.3
+},
+
+{
+name:"Karpuz",
+unit:"100 g",
+calorie:30,
+protein:0.6,
+carb:8,
+fat:0.2
+},
+
+{
+name:"Kavun",
+unit:"100 g",
+calorie:34,
+protein:0.8,
+carb:8,
+fat:0.2
+},
+
+{
+name:"Protein tozu",
+unit:"25 g",
+calorie:110,
+protein:20,
+carb:2,
+fat:1
+},
+
+{
+name:"Whey isolate",
+unit:"25 g",
+calorie:105,
+protein:22,
+carb:1,
+fat:0.5
+},
+
+{
+name:"Kreatin",
+unit:"5 g",
+calorie:0,
+protein:0,
+carb:0,
+fat:0
+},
+
+{
+name:"Protein bar",
+unit:"1 adet",
+calorie:220,
+protein:20,
+carb:22,
+fat:8
+}
+
+];
