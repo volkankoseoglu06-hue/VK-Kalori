c6d2ba978ca549f56963fb788df6a5a7617cabc1
volkankoseoglu06-hue <volkankoseoglu06@gmail.com>
2026-08-14 12:02:30 +0300

    Update foods.js

--- foods.js
+++ foods.js
@@ -60,4 +60,85 @@ const foods = [
 {name:"Latte",kcal:120,protein:6,unit:"bardak"},
 {name:"Maden suyu",kcal:0,protein:0,unit:"şişe"}
 
+  /* ZEYTİNLER */
+
+{name:"Siyah zeytin",kcal:5,protein:0.1,unit:"1 adet"},
+{name:"Yeşil zeytin",kcal:4,protein:0.1,unit:"1 adet"},
+{name:"Zeytin ezmesi",kcal:315,protein:1.5,unit:"100 g"},
+
+/* KURUYEMİŞLER */
+
+{name:"Badem",kcal:7,protein:0.3,unit:"1 adet"},
+{name:"Fındık",kcal:9,protein:0.2,unit:"1 adet"},
+{name:"Ceviz",kcal:26,protein:0.6,unit:"1 adet"},
+{name:"Kaju",kcal:9,protein:0.3,unit:"1 adet"},
+{name:"Antep fıstığı",kcal:4,protein:0.2,unit:"1 adet"},
+{name:"Yer fıstığı",kcal:6,protein:0.3,unit:"1 adet"},
+{name:"Ay çekirdeği",kcal:3,protein:0.1,unit:"1 adet"},
+{name:"Kabak çekirdeği",kcal:5,protein:0.3,unit:"1 adet"},
+{name:"Leblebi",kcal:4,protein:0.2,unit:"1 adet"},
+{name:"Kuru üzüm",kcal:3,protein:0.03,unit:"1 adet"},
+{name:"Kuru kayısı",kcal:17,protein:0.5,unit:"1 adet"},
+{name:"Hurma",kcal:23,protein:0.2,unit:"1 adet"},
+
+/* ŞARKÜTERİ */
+
+{name:"Dana kaburga füme",kcal:175,protein:28,unit:"100 g"},
+{name:"Hindi füme",kcal:110,protein:20,unit:"100 g"},
+{name:"Pastırma",kcal:250,protein:30,unit:"100 g"},
+{name:"Sucuk",kcal:452,protein:24,unit:"100 g"},
+{name:"Salam",kcal:336,protein:17,unit:"100 g"},
+{name:"Sosis",kcal:301,protein:12,unit:"100 g"},
+{name:"Kavurma",kcal:300,protein:24,unit:"100 g"},
+
+/* BALIK */
+
+{name:"Ton balığı",kcal:132,protein:28,unit:"100 g"},
+{name:"Light ton balığı",kcal:110,protein:26,unit:"100 g"},
+{name:"Somon",kcal:208,protein:20,unit:"100 g"},
+{name:"Levrek",kcal:124,protein:24,unit:"100 g"},
+{name:"Çipura",kcal:135,protein:20,unit:"100 g"},
+{name:"Hamsi",kcal:131,protein:20,unit:"100 g"},
+{name:"Uskumru",kcal:205,protein:19,unit:"100 g"},
+
+/* PROTEİN */
+
+{name:"Protein tozu",kcal:100,protein:20,unit:"1 ölçek"},
+{name:"Protein bar",kcal:200,protein:20,unit:"1 adet"},
+{name:"Proteinli puding",kcal:150,protein:20,unit:"1 adet"},
+{name:"Proteinli süt",kcal:180,protein:25,unit:"250 ml"},
+{name:"Kreatin",kcal:0,protein:0,unit:"5 g"},
+
+/* PEYNİRLER */
+
+{name:"Beyaz peynir",kcal:265,protein:14,unit:"100 g"},
+{name:"Kaşar peyniri",kcal:404,protein:25,unit:"100 g"},
+{name:"Lor peyniri",kcal:100,protein:18,unit:"100 g"},
+{name:"Labne",kcal:290,protein:6,unit:"100 g"},
+{name:"Mozzarella",kcal:280,protein:22,unit:"100 g"},
+{name:"Tulum peyniri",kcal:363,protein:22,unit:"100 g"},
+{name:"Çeçil peyniri",kcal:290,protein:27,unit:"100 g"},
+
+/* İÇECEKLER */
+
+{name:"Türk kahvesi",kcal:7,protein:0,unit:"1 fincan"},
+{name:"Americano",kcal:10,protein:0,unit:"1 fincan"},
+{name:"Latte",kcal:120,protein:6,unit:"1 fincan"},
+{name:"Cappuccino",kcal:80,protein:5,unit:"1 fincan"},
+{name:"Çay",kcal:0,protein:0,unit:"1 bardak"},
+{name:"Maden suyu",kcal:0,protein:0,unit:"1 şişe"},
+{name:"Kola",kcal:42,protein:0,unit:"100 ml"},
+{name:"Ayran",kcal:37,protein:2,unit:"100 ml"},
+{name:"Kefir",kcal:60,protein:3.5,unit:"100 ml"},
+
+/* HAZIR YİYECEKLER */
+
+{name:"Lahmacun",kcal:220,protein:11,unit:"1 adet"},
+{name:"Pizza",kcal:285,protein:12,unit:"1 dilim"},
+{name:"Hamburger",kcal:295,protein:17,unit:"1 adet"},
+{name:"Cheeseburger",kcal:320,protein:18,unit:"1 adet"},
+{name:"Börek",kcal:330,protein:9,unit:"1 porsiyon"},
+{name:"Tost",kcal:350,protein:16,unit:"1 adet"},
+{name:"Simit",kcal:272,protein:9,unit:"1 adet"},
+{name:"Pide",kcal:450,protein:20,unit:"1 adet"}
 ];
