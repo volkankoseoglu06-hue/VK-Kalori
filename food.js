5502765c87e23e807e3750bdc5615bc3f1e85e31
volkankoseoglu06-hue <volkankoseoglu06@gmail.com>
2026-08-14 08:46:23 +0300

    Create food.js

---
+++ food.js
@@ -0,0 +1,183 @@
+const foods = [
+
+{
+name:"Karabuğday patlağı",
+unit:"1 dilim",
+gram:10,
+kcal:38,
+protein:1.3,
+carb:8,
+fat:0.4
+},
+
+{
+name:"Ekşi mayalı ekmek",
+unit:"1 dilim",
+gram:35,
+kcal:85,
+protein:3,
+carb:16,
+fat:1
+},
+
+{
+name:"Beyaz peynir",
+unit:"100 g",
+gram:100,
+kcal:265,
+protein:14,
+carb:4,
+fat:21
+},
+
+{
+name:"Kaşar peyniri",
+unit:"100 g",
+gram:100,
+kcal:404,
+protein:25,
+carb:2,
+fat:33
+},
+
+{
+name:"Lor peyniri",
+unit:"100 g",
+gram:100,
+kcal:92,
+protein:18,
+carb:3,
+fat:1
+},
+
+{
+name:"Siyah zeytin",
+unit:"10 adet",
+gram:40,
+kcal:46,
+protein:0.4,
+carb:2.4,
+fat:4.4
+},
+
+{
+name:"Yeşil zeytin",
+unit:"10 adet",
+gram:40,
+kcal:58,
+protein:0.4,
+carb:1.6,
+fat:6
+},
+
+{
+name:"Dana kaburga füme",
+unit:"100 g",
+gram:100,
+kcal:180,
+protein:29,
+carb:1,
+fat:7
+},
+
+{
+name:"Hindi füme",
+unit:"100 g",
+gram:100,
+kcal:110,
+protein:21,
+carb:2,
+fat:2
+},
+
+{
+name:"Izgara tavuk",
+unit:"100 g",
+gram:100,
+kcal:165,
+protein:31,
+carb:0,
+fat:4
+},
+
+{
+name:"Izgara köfte",
+unit:"100 g",
+gram:100,
+kcal:250,
+protein:26,
+carb:5,
+fat:15
+},
+
+{
+name:"Protein tozu",
+unit:"1 ölçek",
+gram:25,
+kcal:120,
+protein:20,
+carb:3,
+fat:2
+},
+
+{
+name:"Fit browni",
+unit:"1 paket",
+gram:45,
+kcal:280,
+protein:22,
+carb:18,
+fat:10
+},
+
+{
+name:"Yoğurt",
+unit:"100 g",
+gram:100,
+kcal:61,
+protein:3.5,
+carb:4.7,
+fat:3.3
+},
+
+{
+name:"Kuru fasulye",
+unit:"100 g",
+gram:100,
+kcal:127,
+protein:9,
+carb:23,
+fat:0.5
+},
+
+{
+name:"Nohut",
+unit:"100 g",
+gram:100,
+kcal:164,
+protein:9,
+carb:27,
+fat:3
+},
+
+{
+name:"Biber dolması",
+unit:"1 adet",
+gram:180,
+kcal:150,
+protein:5,
+carb:19,
+fat:6
+},
+
+{
+name:"Haşlanmış mısır",
+unit:"100 g",
+gram:100,
+kcal:96,
+protein:3.4,
+carb:21,
+fat:1.5
+}
+
+];
