3df9923f18d82c5e438b5267f861c3a0d41be2d9
volkankoseoglu06-hue <volkankoseoglu06@gmail.com>
2026-08-14 10:47:03 +0300

    Update foods.js

--- foods.js
+++ foods.js
@@ -2,86 +2,303 @@ const foods = [
 
 /* KAHVALTILIKLAR */
 
-{name:"Yumurta",kcal:78,protein:6,unit:"adet"},
-{name:"Beyaz peynir",kcal:90,protein:6,unit:"dilim"},
-{name:"Kaşar peyniri",kcal:110,protein:7,unit:"dilim"},
-{name:"Lor peyniri",kcal:60,protein:11,unit:"100 g"},
-{name:"Süzme peynir",kcal:80,protein:8,unit:"100 g"},
-{name:"Siyah zeytin",kcal:45,protein:0,unit:"10 adet"},
-{name:"Yeşil zeytin",kcal:40,protein:0,unit:"10 adet"},
-{name:"Karabuğday patlağı",kcal:19,protein:1,unit:"dilim"},
-{name:"Ekşi mayalı ekmek",kcal:85,protein:3,unit:"dilim"},
-{name:"Tam buğday ekmeği",kcal:70,protein:3,unit:"dilim"},
-{name:"Bal",kcal:64,protein:0,unit:"yemek kaşığı"},
-{name:"Reçel",kcal:55,protein:0,unit:"yemek kaşığı"},
-{name:"Tahin",kcal:89,protein:3,unit:"yemek kaşığı"},
-{name:"Pekmez",kcal:58,protein:0,unit:"yemek kaşığı"},
-{name:"Yulaf",kcal:117,protein:5,unit:"30 g"},
+{
+name:"Yumurta",
+kcal:78,
+protein:6,
+unit:"adet"
+},
+
+{
+name:"Beyaz peynir",
+kcal:90,
+protein:6,
+unit:"dilim"
+},
+
+{
+name:"Kaşar peyniri",
+kcal:110,
+protein:7,
+unit:"dilim"
+},
+
+{
+name:"Lor peyniri",
+kcal:60,
+protein:11,
+unit:"100 g"
+},
+
+{
+name:"Karabuğday patlağı",
+kcal:19,
+protein:1,
+unit:"dilim"
+},
+
+{
+name:"Ekşi mayalı ekmek",
+kcal:85,
+protein:3,
+unit:"dilim"
+},
+
+{
+name:"Yulaf",
+kcal:117,
+protein:5,
+unit:"30 g"
+},
+
+{
+name:"Bal",
+kcal:64,
+protein:0,
+unit:"yemek kaşığı"
+},
 
 /* ET VE TAVUK */
 
-{name:"Izgara tavuk",kcal:165,protein:31,unit:"100 g"},
-{name:"Tavuk göğsü",kcal:165,protein:31,unit:"100 g"},
-{name:"Tavuk şiş",kcal:180,protein:30,unit:"100 g"},
-{name:"Hindi füme",kcal:35,protein:6,unit:"dilim"},
-{name:"Dana kaburga füme",kcal:30,protein:5,unit:"dilim"},
-{name:"Dana eti",kcal:250,protein:26,unit:"100 g"},
-{name:"Köfte",kcal:250,protein:20,unit:"100 g"},
-{name:"Bonfile",kcal:220,protein:28,unit:"100 g"},
-{name:"Antrikot",kcal:291,protein:25,unit:"100 g"},
-{name:"Ton balığı",kcal:132,protein:28,unit:"100 g"},
-{name:"Somon",kcal:208,protein:22,unit:"100 g"},
-{name:"Sucuk",kcal:135,protein:6,unit:"30 g"},
+{
+name:"Izgara tavuk",
+kcal:165,
+protein:31,
+unit:"100 g"
+},
+
+{
+name:"Tavuk göğsü",
+kcal:165,
+protein:31,
+unit:"100 g"
+},
+
+{
+name:"Tavuk şiş",
+kcal:180,
+protein:30,
+unit:"100 g"
+},
+
+{
+name:"Dana kaburga füme",
+kcal:30,
+protein:5,
+unit:"dilim"
+},
+
+{
+name:"Hindi füme",
+kcal:35,
+protein:6,
+unit:"dilim"
+},
+
+{
+name:"Köfte",
+kcal:250,
+protein:20,
+unit:"100 g"
+},
+
+{
+name:"Ton balığı",
+kcal:132,
+protein:28,
+unit:"100 g"
+},
+
+{
+name:"Somon",
+kcal:208,
+protein:22,
+unit:"100 g"
+},
 
 /* SÜT ÜRÜNLERİ */
 
-{name:"Yoğurt",kcal:120,protein:8,unit:"kase"},
-{name:"Süzme yoğurt",kcal:150,protein:10,unit:"kase"},
-{name:"Ayran",kcal:68,protein:3,unit:"bardak"},
-{name:"Kefir",kcal:110,protein:6,unit:"bardak"},
-{name:"Süt",kcal:122,protein:8,unit:"bardak"},
-{name:"Laktozsuz süt",kcal:120,protein:8,unit:"bardak"},
+{
+name:"Yoğurt",
+kcal:120,
+protein:8,
+unit:"kase"
+},
+
+{
+name:"Süzme yoğurt",
+kcal:150,
+protein:10,
+unit:"kase"
+},
+
+{
+name:"Ayran",
+kcal:68,
+protein:3,
+unit:"bardak"
+},
+
+{
+name:"Kefir",
+kcal:110,
+protein:6,
+unit:"bardak"
+},
+
+{
+name:"Süt",
+kcal:122,
+protein:8,
+unit:"bardak"
+},
 
 /* BAKLİYAT */
 
-{name:"Kuru fasulye",kcal:170,protein:10,unit:"porsiyon"},
-{name:"Nohut",kcal:180,protein:9,unit:"porsiyon"},
-{name:"Yeşil mercimek",kcal:190,protein:13,unit:"porsiyon"},
-{name:"Kırmızı mercimek",kcal:180,protein:12,unit:"porsiyon"},
-{name:"Barbunya",kcal:175,protein:9,unit:"porsiyon"},
-{name:"Börülce",kcal:170,protein:10,unit:"porsiyon"},
+{
+name:"Kuru fasulye",
+kcal:170,
+protein:10,
+unit:"porsiyon"
+},
+
+{
+name:"Nohut",
+kcal:180,
+protein:9,
+unit:"porsiyon"
+},
+
+{
+name:"Yeşil mercimek",
+kcal:190,
+protein:13,
+unit:"porsiyon"
+},
+
+{
+name:"Barbunya",
+kcal:175,
+protein:9,
+unit:"porsiyon"
+},
 
 /* KARBONHİDRATLAR */
 
-{name:"Pirinç pilavı",kcal:220,protein:4,unit:"porsiyon"},
-{name:"Bulgur pilavı",kcal:180,protein:5,unit:"porsiyon"},
-{name:"Makarna",kcal:220,protein:8,unit:"porsiyon"},
-{name:"Tam buğday makarna",kcal:210,protein:9,unit:"porsiyon"},
-{name:"Patates",kcal:160,protein:4,unit:"porsiyon"},
-{name:"Fırın patates",kcal:180,protein:4,unit:"porsiyon"},
-{name:"Pirinç",kcal:130,protein:3,unit:"100 g"},
-{name:"Bulgur",kcal:110,protein:4,unit:"100 g"},
+{
+name:"Pirinç pilavı",
+kcal:220,
+protein:4,
+unit:"porsiyon"
+},
+
+{
+name:"Bulgur pilavı",
+kcal:180,
+protein:5,
+unit:"porsiyon"
+},
+
+{
+name:"Makarna",
+kcal:220,
+protein:8,
+unit:"porsiyon"
+},
+
+{
+name:"Patates",
+kcal:160,
+protein:4,
+unit:"porsiyon"
+},
+
+{
+name:"Fırın patates",
+kcal:180,
+protein:4,
+unit:"porsiyon"
+},
 
 /* MEYVELER */
 
-{name:"Elma",kcal:95,protein:0,unit:"adet"},
-{name:"Muz",kcal:105,protein:1,unit:"adet"},
-{name:"Portakal",kcal:62,protein:1,unit:"adet"},
-{name:"Mandalina",kcal:47,protein:1,unit:"adet"},
-{name:"Çilek",kcal:49,protein:1,unit:"100 g"},
-{name:"Şeftali",kcal:59,protein:1,unit:"adet"},
-{name:"Karpuz",kcal:85,protein:2,unit:"dilim"},
-{name:"Kavun",kcal:60,protein:1,unit:"dilim"},
-{name:"Üzüm",kcal:62,protein:1,unit:"100 g"},
-{name:"Kayısı",kcal:17,protein:0,unit:"adet"},
+{
+name:"Elma",
+kcal:95,
+protein:0,
+unit:"adet"
+},
+
+{
+name:"Muz",
+kcal:105,
+protein:1,
+unit:"adet"
+},
+
+{
+name:"Portakal",
+kcal:62,
+protein:1,
+unit:"adet"
+},
+
+{
+name:"Mandalina",
+kcal:47,
+protein:1,
+unit:"adet"
+},
+
+{
+name:"Çilek",
+kcal:49,
+protein:1,
+unit:"100 g"
+},
+
+{
+name:"Karpuz",
+kcal:85,
+protein:2,
+unit:"dilim"
+},
 
 /* SPOR BESİNLERİ */
 
-{name:"Protein tozu",kcal:120,protein:20,unit:"ölçek"},
-{name:"Whey protein",kcal:120,protein:24,unit:"ölçek"},
-{name:"Protein bar",kcal:220,protein:20,unit:"adet"},
-{name:"Fit browni",kcal:280,protein:20,unit:"adet"},
-{name:"Yüksek proteinli puding",kcal:150,protein:20,unit:"adet"},
-{name:"Kreatin içeceği",kcal:20,protein:0,unit:"ölçek"}
+{
+name:"Protein tozu",
+kcal:120,
+protein:20,
+unit:"ölçek"
+},
+
+{
+name:"Whey protein",
+kcal:120,
+protein:24,
+unit:"ölçek"
+},
+
+{
+name:"Protein bar",
+kcal:220,
+protein:20,
+unit:"adet"
+},
+
+{
+name:"Fit browni",
+kcal:280,
+protein:20,
+unit:"adet"
+},
+
+{
+name:"Yüksek proteinli puding",
+kcal:150,
+protein:20,
+unit:"adet"
+}
 
 ];
