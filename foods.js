f5d11c8548991767ed7c15b7c110e71630b5f48f
volkankoseoglu06-hue <volkankoseoglu06@gmail.com>
2026-08-14 11:15:13 +0300

    Update foods.js

--- foods.js
+++ foods.js
@@ -1,77 +1,59 @@
 const foods = [
 
-/* KAHVALTILIKLAR */
-
 {name:"Yumurta",kcal:78,protein:6,unit:"adet"},
-{name:"Haşlanmış yumurta",kcal:78,protein:6,unit:"adet"},
-{name:"Omlet",kcal:180,protein:13,unit:"porsiyon"},
-{name:"Menemen",kcal:210,protein:12,unit:"porsiyon"},
-{name:"Beyaz peynir",kcal:90,protein:6,unit:"dilim"},
-{name:"Kaşar peyniri",kcal:110,protein:7,unit:"dilim"},
-{name:"Lor peyniri",kcal:60,protein:11,unit:"100 g"},
+{name:"Beyaz peynir",kcal:90,protein:6,unit:"30 g"},
+{name:"Kaşar peyniri",kcal:110,protein:7,unit:"30 g"},
+{name:"Lor peyniri",kcal:98,protein:18,unit:"100 g"},
 {name:"Karabuğday patlağı",kcal:19,protein:1,unit:"adet"},
-{name:"Yulaf",kcal:117,protein:5,unit:"30 g"},
 {name:"Ekşi mayalı ekmek",kcal:85,protein:3,unit:"dilim"},
-
-/* TAVUK VE ET */
+{name:"Tam buğday ekmeği",kcal:70,protein:3,unit:"dilim"},
+{name:"Yulaf",kcal:389,protein:17,unit:"100 g"},
+{name:"Menemen",kcal:210,protein:12,unit:"porsiyon"},
 
 {name:"Izgara tavuk",kcal:165,protein:31,unit:"100 g"},
 {name:"Tavuk göğsü",kcal:165,protein:31,unit:"100 g"},
 {name:"Tavuk şiş",kcal:180,protein:30,unit:"100 g"},
 {name:"Tavuk döner",kcal:215,protein:22,unit:"100 g"},
-{name:"Hindi füme",kcal:35,protein:6,unit:"dilim"},
-{name:"Dana kaburga füme",kcal:30,protein:5,unit:"dilim"},
+{name:"Hindi füme",kcal:110,protein:19,unit:"100 g"},
+{name:"Dana kaburga füme",kcal:175,protein:28,unit:"100 g"},
 {name:"Köfte",kcal:250,protein:20,unit:"100 g"},
 {name:"Biftek",kcal:220,protein:28,unit:"100 g"},
 {name:"Et döner",kcal:290,protein:23,unit:"100 g"},
-
-/* BALIK */
 
 {name:"Ton balığı",kcal:132,protein:28,unit:"100 g"},
 {name:"Somon",kcal:208,protein:22,unit:"100 g"},
 {name:"Levrek",kcal:124,protein:23,unit:"100 g"},
 {name:"Çipura",kcal:135,protein:22,unit:"100 g"},
 
-/* SÜT ÜRÜNLERİ */
-
-{name:"Yoğurt",kcal:120,protein:8,unit:"kase"},
-{name:"Süzme yoğurt",kcal:150,protein:10,unit:"kase"},
-{name:"Ayran",kcal:68,protein:3,unit:"bardak"},
-{name:"Kefir",kcal:110,protein:6,unit:"bardak"},
-{name:"Süt",kcal:122,protein:8,unit:"bardak"},
-
-/* BAKLİYAT */
+{name:"Yoğurt",kcal:61,protein:4,unit:"100 g"},
+{name:"Süzme yoğurt",kcal:97,protein:10,unit:"100 g"},
+{name:"Kefir",kcal:60,protein:3,unit:"100 ml"},
+{name:"Ayran",kcal:37,protein:2,unit:"100 ml"},
+{name:"Süt",kcal:61,protein:3,unit:"100 ml"},
 
 {name:"Kuru fasulye",kcal:170,protein:10,unit:"porsiyon"},
 {name:"Nohut",kcal:180,protein:9,unit:"porsiyon"},
 {name:"Yeşil mercimek",kcal:190,protein:13,unit:"porsiyon"},
 {name:"Mercimek çorbası",kcal:150,protein:8,unit:"kase"},
 
-/* KARBONHİDRATLAR */
-
 {name:"Pirinç pilavı",kcal:220,protein:4,unit:"porsiyon"},
 {name:"Bulgur pilavı",kcal:180,protein:5,unit:"porsiyon"},
 {name:"Makarna",kcal:220,protein:8,unit:"porsiyon"},
-{name:"Patates",kcal:160,protein:4,unit:"porsiyon"},
-{name:"Fırın patates",kcal:180,protein:4,unit:"porsiyon"},
-
-/* MEYVELER */
+{name:"Patates",kcal:77,protein:2,unit:"100 g"},
+{name:"Fırın patates",kcal:93,protein:2,unit:"100 g"},
+{name:"Patates kızartması",kcal:312,protein:3,unit:"100 g"},
 
 {name:"Elma",kcal:95,protein:0,unit:"adet"},
 {name:"Muz",kcal:105,protein:1,unit:"adet"},
 {name:"Portakal",kcal:62,protein:1,unit:"adet"},
 {name:"Mandalina",kcal:47,protein:1,unit:"adet"},
-{name:"Çilek",kcal:49,protein:1,unit:"100 g"},
-{name:"Karpuz",kcal:85,protein:2,unit:"dilim"},
+{name:"Çilek",kcal:32,protein:1,unit:"100 g"},
+{name:"Karpuz",kcal:30,protein:1,unit:"100 g"},
 
-/* SPOR BESİNLERİ */
-
-{name:"Protein tozu",kcal:120,protein:20,unit:"ölçek"},
+{name:"Protein tozu",kcal:120,protein:24,unit:"ölçek"},
 {name:"Whey protein",kcal:120,protein:24,unit:"ölçek"},
 {name:"Protein bar",kcal:220,protein:20,unit:"adet"},
-{name:"Yüksek proteinli puding",kcal:150,protein:20,unit:"adet"},
-
-/* İÇECEKLER */
+{name:"Proteinli puding",kcal:150,protein:20,unit:"adet"},
 
 {name:"Çay",kcal:2,protein:0,unit:"bardak"},
 {name:"Türk kahvesi",kcal:7,protein:0,unit:"fincan"},
