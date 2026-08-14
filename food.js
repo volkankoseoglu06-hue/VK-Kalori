7ac2b0b42b44df198f35161fb5386fb5d040e19c
volkankoseoglu06-hue <volkankoseoglu06@gmail.com>
2026-08-14 08:48:44 +0300

    Update food.js

--- food.js
+++ food.js
@@ -1,183 +1,115 @@
 const foods = [
 
-{
-name:"Karabuğday patlağı",
-unit:"1 dilim",
-gram:10,
-kcal:38,
-protein:1.3,
-carb:8,
-fat:0.4
-},
+/* 🥞 Kahvaltılıklar */
 
-{
-name:"Ekşi mayalı ekmek",
-unit:"1 dilim",
-gram:35,
-kcal:85,
-protein:3,
-carb:16,
-fat:1
-},
+{name:"Karabuğday patlağı",gram:10,unit:"1 dilim",kcal:38,protein:1.3,carb:8,fat:0.4},
 
-{
-name:"Beyaz peynir",
-unit:"100 g",
-gram:100,
-kcal:265,
-protein:14,
-carb:4,
-fat:21
-},
+{name:"Ekşi mayalı ekmek",gram:35,unit:"1 dilim",kcal:85,protein:3,carb:16,fat:1},
 
-{
-name:"Kaşar peyniri",
-unit:"100 g",
-gram:100,
-kcal:404,
-protein:25,
-carb:2,
-fat:33
-},
+{name:"Tam buğday ekmeği",gram:30,unit:"1 dilim",kcal:75,protein:3,carb:14,fat:1},
 
-{
-name:"Lor peyniri",
-unit:"100 g",
-gram:100,
-kcal:92,
-protein:18,
-carb:3,
-fat:1
-},
+{name:"Yumurta",gram:50,unit:"1 adet",kcal:78,protein:6.5,carb:0.6,fat:5},
 
-{
-name:"Siyah zeytin",
-unit:"10 adet",
-gram:40,
-kcal:46,
-protein:0.4,
-carb:2.4,
-fat:4.4
-},
+{name:"Haşlanmış yumurta",gram:50,unit:"1 adet",kcal:78,protein:6.5,carb:0.6,fat:5},
 
-{
-name:"Yeşil zeytin",
-unit:"10 adet",
-gram:40,
-kcal:58,
-protein:0.4,
-carb:1.6,
-fat:6
-},
+{name:"Omlet",gram:100,unit:"100 g",kcal:154,protein:11,carb:1.9,fat:11},
 
-{
-name:"Dana kaburga füme",
-unit:"100 g",
-gram:100,
-kcal:180,
-protein:29,
-carb:1,
-fat:7
-},
+/* 🧀 Peynirler */
 
-{
-name:"Hindi füme",
-unit:"100 g",
-gram:100,
-kcal:110,
-protein:21,
-carb:2,
-fat:2
-},
+{name:"Beyaz peynir",gram:100,unit:"100 g",kcal:265,protein:14,carb:4,fat:21},
 
-{
-name:"Izgara tavuk",
-unit:"100 g",
-gram:100,
-kcal:165,
-protein:31,
-carb:0,
-fat:4
-},
+{name:"Kaşar peyniri",gram:100,unit:"100 g",kcal:404,protein:25,carb:2,fat:33},
 
-{
-name:"Izgara köfte",
-unit:"100 g",
-gram:100,
-kcal:250,
-protein:26,
-carb:5,
-fat:15
-},
+{name:"Lor peyniri",gram:100,unit:"100 g",kcal:92,protein:18,carb:3,fat:1},
 
-{
-name:"Protein tozu",
-unit:"1 ölçek",
-gram:25,
-kcal:120,
-protein:20,
-carb:3,
-fat:2
-},
+{name:"Ezine peyniri",gram:100,unit:"100 g",kcal:289,protein:16,carb:2,fat:24},
 
-{
-name:"Fit browni",
-unit:"1 paket",
-gram:45,
-kcal:280,
-protein:22,
-carb:18,
-fat:10
-},
+{name:"Tulum peyniri",gram:100,unit:"100 g",kcal:356,protein:25,carb:2,fat:28},
 
-{
-name:"Yoğurt",
-unit:"100 g",
-gram:100,
-kcal:61,
-protein:3.5,
-carb:4.7,
-fat:3.3
-},
+/* 🫒 Zeytinler */
 
-{
-name:"Kuru fasulye",
-unit:"100 g",
-gram:100,
-kcal:127,
-protein:9,
-carb:23,
-fat:0.5
-},
+{name:"Siyah zeytin",gram:40,unit:"10 adet",kcal:46,protein:0.4,carb:2.4,fat:4.4},
 
-{
-name:"Nohut",
-unit:"100 g",
-gram:100,
-kcal:164,
-protein:9,
-carb:27,
-fat:3
-},
+{name:"Yeşil zeytin",gram:40,unit:"10 adet",kcal:58,protein:0.4,carb:1.6,fat:6},
 
-{
-name:"Biber dolması",
-unit:"1 adet",
-gram:180,
-kcal:150,
-protein:5,
-carb:19,
-fat:6
-},
+/* 🥩 Fümeler */
 
-{
-name:"Haşlanmış mısır",
-unit:"100 g",
-gram:100,
-kcal:96,
-protein:3.4,
-carb:21,
-fat:1.5
-}
+{name:"Dana kaburga füme",gram:100,unit:"100 g",kcal:180,protein:29,carb:1,fat:7},
+
+{name:"Hindi füme",gram:100,unit:"100 g",kcal:110,protein:21,carb:2,fat:2},
+
+{name:"Et füme",gram:100,unit:"100 g",kcal:170,protein:28,carb:1,fat:6},
+
+/* 🍗 Izgaralar */
+
+{name:"Izgara tavuk",gram:100,unit:"100 g",kcal:165,protein:31,carb:0,fat:4},
+
+{name:"Tavuk şiş",gram:100,unit:"100 g",kcal:180,protein:29,carb:1,fat:5},
+
+{name:"Izgara köfte",gram:100,unit:"100 g",kcal:250,protein:26,carb:5,fat:15},
+
+{name:"Izgara hindi",gram:100,unit:"100 g",kcal:135,protein:30,carb:0,fat:2},
+
+/* 🥣 Bakliyatlar */
+
+{name:"Kuru fasulye",gram:100,unit:"100 g",kcal:127,protein:9,carb:23,fat:0.5},
+
+{name:"Nohut",gram:100,unit:"100 g",kcal:164,protein:9,carb:27,fat:3},
+
+{name:"Yeşil mercimek",gram:100,unit:"100 g",kcal:116,protein:9,carb:20,fat:0.4},
+
+/* 🌽 Sebzeler */
+
+{name:"Haşlanmış mısır",gram:100,unit:"100 g",kcal:96,protein:3.4,carb:21,fat:1.5},
+
+{name:"Biber dolması",gram:180,unit:"1 adet",kcal:150,protein:5,carb:19,fat:6},
+
+{name:"Patlıcan yemeği",gram:100,unit:"100 g",kcal:89,protein:1.5,carb:8,fat:5},
+
+/* 🍚 Pilavlar */
+
+{name:"Pirinç pilavı",gram:100,unit:"100 g",kcal:130,protein:2.7,carb:28,fat:0.3},
+
+{name:"Bulgur pilavı",gram:100,unit:"100 g",kcal:83,protein:3,carb:19,fat:0.2},
+
+/* 🍝 Makarnalar */
+
+{name:"Makarna",gram:100,unit:"100 g",kcal:158,protein:6,carb:31,fat:1},
+
+{name:"Tam buğday makarna",gram:100,unit:"100 g",kcal:149,protein:6.5,carb:30,fat:1},
+
+/* 🥜 Kuruyemişler */
+
+{name:"Badem",gram:100,unit:"100 g",kcal:579,protein:21,carb:22,fat:50},
+
+{name:"Fındık",gram:100,unit:"100 g",kcal:628,protein:15,carb:17,fat:61},
+
+{name:"Ceviz",gram:100,unit:"100 g",kcal:654,protein:15,carb:14,fat:65},
+
+/* 🍫 Protein ürünleri */
+
+{name:"Protein tozu",gram:25,unit:"1 ölçek",kcal:120,protein:20,carb:3,fat:2},
+
+{name:"Whey protein",gram:25,unit:"1 ölçek",kcal:120,protein:20,carb:3,fat:2},
+
+{name:"Protein bar",gram:60,unit:"1 adet",kcal:220,protein:20,carb:18,fat:8},
+
+{name:"Fit browni",gram:45,unit:"1 paket",kcal:280,protein:22,carb:18,fat:10},
+
+/* 🥛 Süt ürünleri */
+
+{name:"Yoğurt",gram:100,unit:"100 g",kcal:61,protein:3.5,carb:4.7,fat:3.3},
+
+{name:"Süzme yoğurt",gram:100,unit:"100 g",kcal:97,protein:9,carb:3.5,fat:5},
+
+{name:"Ayran",gram:200,unit:"1 bardak",kcal:68,protein:4,carb:5,fat:3},
+
+/* 🍎 Meyveler */
+
+{name:"Elma",gram:100,unit:"100 g",kcal:52,protein:0.3,carb:14,fat:0.2},
+
+{name:"Muz",gram:100,unit:"100 g",kcal:89,protein:1.1,carb:23,fat:0.3},
+
+{name:"Portakal",gram:100,unit:"100 g",kcal:47,protein:0.9,carb:12,fat:0.1}
 
 ];
