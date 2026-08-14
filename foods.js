95a0dde7337edb9da865cdf8bf4fcd7800c14e8e
volkankoseoglu06-hue <volkankoseoglu06@gmail.com>
2026-08-14 11:25:49 +0300

    Update foods.js

--- foods.js
+++ foods.js
@@ -1,14 +1,16 @@
 const foods = [
 
 {name:"Yumurta",kcal:78,protein:6,unit:"adet"},
+{name:"Yumurta beyazı",kcal:17,protein:4,unit:"adet"},
 {name:"Beyaz peynir",kcal:90,protein:6,unit:"30 g"},
 {name:"Kaşar peyniri",kcal:110,protein:7,unit:"30 g"},
 {name:"Lor peyniri",kcal:98,protein:18,unit:"100 g"},
-{name:"Karabuğday patlağı",kcal:19,protein:1,unit:"adet"},
-{name:"Ekşi mayalı ekmek",kcal:85,protein:3,unit:"dilim"},
-{name:"Tam buğday ekmeği",kcal:70,protein:3,unit:"dilim"},
-{name:"Yulaf",kcal:389,protein:17,unit:"100 g"},
-{name:"Menemen",kcal:210,protein:12,unit:"porsiyon"},
+{name:"Labne",kcal:230,protein:8,unit:"100 g"},
+{name:"Yoğurt",kcal:61,protein:4,unit:"100 g"},
+{name:"Süzme yoğurt",kcal:97,protein:10,unit:"100 g"},
+{name:"Ayran",kcal:37,protein:2,unit:"100 ml"},
+{name:"Süt",kcal:61,protein:3,unit:"100 ml"},
+{name:"Kefir",kcal:60,protein:3,unit:"100 ml"},
 
 {name:"Izgara tavuk",kcal:165,protein:31,unit:"100 g"},
 {name:"Tavuk göğsü",kcal:165,protein:31,unit:"100 g"},
@@ -16,8 +18,8 @@ const foods = [
 {name:"Tavuk döner",kcal:215,protein:22,unit:"100 g"},
 {name:"Hindi füme",kcal:110,protein:19,unit:"100 g"},
 {name:"Dana kaburga füme",kcal:175,protein:28,unit:"100 g"},
-{name:"Köfte",kcal:250,protein:20,unit:"100 g"},
 {name:"Biftek",kcal:220,protein:28,unit:"100 g"},
+{name:"Köfte",kcal:250,protein:20,unit:"100 g"},
 {name:"Et döner",kcal:290,protein:23,unit:"100 g"},
 
 {name:"Ton balığı",kcal:132,protein:28,unit:"100 g"},
@@ -25,23 +27,21 @@ const foods = [
 {name:"Levrek",kcal:124,protein:23,unit:"100 g"},
 {name:"Çipura",kcal:135,protein:22,unit:"100 g"},
 
-{name:"Yoğurt",kcal:61,protein:4,unit:"100 g"},
-{name:"Süzme yoğurt",kcal:97,protein:10,unit:"100 g"},
-{name:"Kefir",kcal:60,protein:3,unit:"100 ml"},
-{name:"Ayran",kcal:37,protein:2,unit:"100 ml"},
-{name:"Süt",kcal:61,protein:3,unit:"100 ml"},
-
-{name:"Kuru fasulye",kcal:170,protein:10,unit:"porsiyon"},
-{name:"Nohut",kcal:180,protein:9,unit:"porsiyon"},
-{name:"Yeşil mercimek",kcal:190,protein:13,unit:"porsiyon"},
-{name:"Mercimek çorbası",kcal:150,protein:8,unit:"kase"},
-
+{name:"Karabuğday patlağı",kcal:19,protein:1,unit:"adet"},
+{name:"Ekşi mayalı ekmek",kcal:85,protein:3,unit:"dilim"},
+{name:"Tam buğday ekmeği",kcal:70,protein:3,unit:"dilim"},
+{name:"Yulaf",kcal:389,protein:17,unit:"100 g"},
 {name:"Pirinç pilavı",kcal:220,protein:4,unit:"porsiyon"},
 {name:"Bulgur pilavı",kcal:180,protein:5,unit:"porsiyon"},
 {name:"Makarna",kcal:220,protein:8,unit:"porsiyon"},
 {name:"Patates",kcal:77,protein:2,unit:"100 g"},
 {name:"Fırın patates",kcal:93,protein:2,unit:"100 g"},
 {name:"Patates kızartması",kcal:312,protein:3,unit:"100 g"},
+
+{name:"Kuru fasulye",kcal:170,protein:10,unit:"porsiyon"},
+{name:"Nohut",kcal:180,protein:9,unit:"porsiyon"},
+{name:"Yeşil mercimek",kcal:190,protein:13,unit:"porsiyon"},
+{name:"Mercimek çorbası",kcal:150,protein:8,unit:"kase"},
 
 {name:"Elma",kcal:95,protein:0,unit:"adet"},
 {name:"Muz",kcal:105,protein:1,unit:"adet"},
@@ -51,7 +51,6 @@ const foods = [
 {name:"Karpuz",kcal:30,protein:1,unit:"100 g"},
 
 {name:"Protein tozu",kcal:120,protein:24,unit:"ölçek"},
-{name:"Whey protein",kcal:120,protein:24,unit:"ölçek"},
 {name:"Protein bar",kcal:220,protein:20,unit:"adet"},
 {name:"Proteinli puding",kcal:150,protein:20,unit:"adet"},
 
