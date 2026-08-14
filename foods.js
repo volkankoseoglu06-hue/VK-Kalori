d1f16651dc6ca2819636066b9170d51b55c56bc6
volkankoseoglu06-hue <volkankoseoglu06@gmail.com>
2026-08-14 09:07:05 +0300

    Add files via upload

---
+++ foods.js
@@ -0,0 +1,7 @@
+const foods=[
+{name:"Karabuğday patlağı",kcal:38},{name:"Protein tozu",kcal:120},
+{name:"Izgara tavuk",kcal:165},{name:"Dana kaburga füme",kcal:180},
+{name:"Beyaz peynir",kcal:265},{name:"Yoğurt",kcal:61},
+{name:"Kuru fasulye",kcal:127},{name:"Nohut",kcal:164},
+{name:"Ekşi mayalı ekmek",kcal:85},{name:"Muz",kcal:89}
+];
